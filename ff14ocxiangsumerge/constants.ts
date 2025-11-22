f5387fb02b9e61a9f3e83a48d3977b31afd4da10
ribbaty <ribbaty@gmail.com>
2025-11-22 11:28:06 +0800

    Update constants.ts

--- ff14ocxiangsumerge/constants.ts
+++ ff14ocxiangsumerge/constants.ts
@@ -7,8 +7,8 @@ export const GAME_HEIGHT = 650; // Logical height
 export const DEADLINE_Y = 150; // Y position of the game over line
 
 // TODO: Replace this string with the URL of your uploaded image to see it in the game!
-// Example: '/background.png' (if in public folder) or 'https://github.com/ribbaty/first/blob/main/lovelyduoduo.png?raw=true'
-export const BACKGROUND_IMAGE_URL = "https://github.com/ribbaty/first/blob/main/lovelyduoduo.png?raw=true"; 
+// Example: '/background.png' (if in public folder) or 'https://raw.githubusercontent.com/ribbaty/three/83aed0deb9dec366bfcc65222fedd812e13e849b/ff14ocxiangsumerge/beijing.png'
+export const BACKGROUND_IMAGE_URL = "https://raw.githubusercontent.com/ribbaty/three/83aed0deb9dec366bfcc65222fedd812e13e849b/ff14ocxiangsumerge/beijing.png"; 
 
 // Using Microsoft Fluent UI 3D Emojis hosted on CDN for demonstration.
 // These are PNGs with transparent backgrounds.
@@ -23,7 +23,7 @@ export const FRUITS: FruitType[] = [
     emoji: '🎀', 
     score: 2,
     // Replaced with a Data URI representing a Pink Bow to match your request immediately
-    imgUrl: 'https://github.com/ribbaty/first/blob/1e44308d25c062b137edca94365c31284973a696/00.png?raw=true',
+    imgUrl: 'https://raw.githubusercontent.com/ribbaty/three/83aed0deb9dec366bfcc65222fedd812e13e849b/ff14ocxiangsumerge/00.png',
     hitboxScale: 0.75 // Tighter hitbox for irregular shape
   },
   { 
@@ -33,7 +33,7 @@ export const FRUITS: FruitType[] = [
     color: '#DC2626', 
     emoji: '🍒', 
     score: 4,
-    imgUrl: 'https://github.com/ribbaty/first/blob/1e44308d25c062b137edca94365c31284973a696/01.png?raw=true',
+    imgUrl: 'https://raw.githubusercontent.com/ribbaty/three/83aed0deb9dec366bfcc65222fedd812e13e849b/ff14ocxiangsumerge/01.png',
     hitboxScale: 0.85
   },
   { 
@@ -43,7 +43,7 @@ export const FRUITS: FruitType[] = [
     color: '#EA580C', 
     emoji: '🍊', 
     score: 8,
-    imgUrl: 'https://github.com/ribbaty/first/blob/1e44308d25c062b137edca94365c31284973a696/02.png?raw=true',
+    imgUrl: 'https://raw.githubusercontent.com/ribbaty/three/83aed0deb9dec366bfcc65222fedd812e13e849b/ff14ocxiangsumerge/02.png',
     hitboxScale: 0.9
   },
   { 
@@ -53,7 +53,7 @@ export const FRUITS: FruitType[] = [
     color: '#FACC15', 
     emoji: '🍋', 
     score: 16,
-    imgUrl: 'https://github.com/ribbaty/first/blob/1e44308d25c062b137edca94365c31284973a696/03.png?raw=true',
+    imgUrl: 'https://raw.githubusercontent.com/ribbaty/three/83aed0deb9dec366bfcc65222fedd812e13e849b/ff14ocxiangsumerge/03.png',
     hitboxScale: 0.85
   },
   { 
@@ -63,7 +63,7 @@ export const FRUITS: FruitType[] = [
     color: '#65A30D', 
     emoji: '🥝', 
     score: 32,
-    imgUrl: 'https://github.com/ribbaty/first/blob/1e44308d25c062b137edca94365c31284973a696/04.png?raw=true',
+    imgUrl: 'https://raw.githubusercontent.com/ribbaty/three/83aed0deb9dec366bfcc65222fedd812e13e849b/ff14ocxiangsumerge/04.png',
     hitboxScale: 0.9
   },
   { 
@@ -73,7 +73,7 @@ export const FRUITS: FruitType[] = [
     color: '#E11D48', 
     emoji: '🍅', 
     score: 64,
-    imgUrl: 'https://github.com/ribbaty/first/blob/1e44308d25c062b137edca94365c31284973a696/05.png?raw=true',
+    imgUrl: 'https://raw.githubusercontent.com/ribbaty/three/83aed0deb9dec366bfcc65222fedd812e13e849b/ff14ocxiangsumerge/05.png',
     hitboxScale: 0.9
   },
   { 
@@ -83,7 +83,7 @@ export const FRUITS: FruitType[] = [
     color: '#F472B6', 
     emoji: '🍑', 
     score: 128,
-    imgUrl: 'https://github.com/ribbaty/first/blob/1e44308d25c062b137edca94365c31284973a696/06.png?raw=true',
+    imgUrl: 'https://raw.githubusercontent.com/ribbaty/three/83aed0deb9dec366bfcc65222fedd812e13e849b/ff14ocxiangsumerge/06.png',
     hitboxScale: 0.9
   },
   { 
@@ -93,7 +93,7 @@ export const FRUITS: FruitType[] = [
     color: '#F59E0B', 
     emoji: '🍍', 
     score: 256,
-    imgUrl: 'https://github.com/ribbaty/first/blob/1e44308d25c062b137edca94365c31284973a696/07.png?raw=true',
+    imgUrl: 'https://raw.githubusercontent.com/ribbaty/three/83aed0deb9dec366bfcc65222fedd812e13e849b/ff14ocxiangsumerge/07.png',
     hitboxScale: 0.85
   },
   { 
@@ -103,7 +103,7 @@ export const FRUITS: FruitType[] = [
     color: '#78350F', 
     emoji: '🥥', 
     score: 512,
-    imgUrl: 'https://github.com/ribbaty/first/blob/1e44308d25c062b137edca94365c31284973a696/08.png?raw=true',
+    imgUrl: 'https://raw.githubusercontent.com/ribbaty/three/83aed0deb9dec366bfcc65222fedd812e13e849b/ff14ocxiangsumerge/08.png',
     hitboxScale: 0.9
   },
   { 
@@ -113,7 +113,7 @@ export const FRUITS: FruitType[] = [
     color: '#10B981', 
     emoji: '🍉', 
     score: 1024,
-    imgUrl: 'https://github.com/ribbaty/first/blob/1e44308d25c062b137edca94365c31284973a696/09.png?raw=true',
+    imgUrl: 'https://raw.githubusercontent.com/ribbaty/three/83aed0deb9dec366bfcc65222fedd812e13e849b/ff14ocxiangsumerge/09.png',
     hitboxScale: 0.9
   },
   { 
@@ -123,7 +123,7 @@ export const FRUITS: FruitType[] = [
     color: '#059669', 
     emoji: '🏆', 
     score: 2048,
-    imgUrl: 'https://github.com/ribbaty/first/blob/1e44308d25c062b137edca94365c31284973a696/10.png?raw=true',
+    imgUrl: 'https://raw.githubusercontent.com/ribbaty/three/83aed0deb9dec366bfcc65222fedd812e13e849b/ff14ocxiangsumerge/10.png',
     hitboxScale: 0.9
   },
 ];
